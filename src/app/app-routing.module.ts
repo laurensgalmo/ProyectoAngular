adf17e50ca252d715cbf0e9235236f7c7451b766
Galvez Moya <laura.galvez.moya@accenture.com>
2023-02-17 14:49:32 +0100

    modificaciones

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CategoriasComponent } from './categorias/categorias.component';
-import { InicioComponent } from './inicio/inicio.component';
-
 
 const routes: Routes = [
-  {path:'inicio', component:InicioComponent},
   {path:'categorias', component:CategoriasComponent}
- // {path:'contacto', component:contactoComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { CategoriasComponent } from './categorias/categorias.component';
-import { InicioComponent } from './inicio/inicio.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
@@ -13,7 +12,6 @@ import { FormsModule } from '@angular/forms';
     AppComponent,
     MenuComponent,
     CategoriasComponent,
-    InicioComponent
     //FALTA CONTACTO
   ],
   imports: [
